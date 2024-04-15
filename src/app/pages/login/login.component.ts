04851a10aba5d7c17bc97851cc1feb9d6b6072aa
FCT-Marcos <164381752+FCT-Marcos@users.noreply.github.com>
2024-04-15 22:38:51 +0100

    Added login form logic and validation

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,51 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { RouterLink, Router } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators  } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth/auth.service'
+import { LoginRequest } from '../../interfaces/LoginRequest';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, RouterLink, FormsModule, ReactiveFormsModule ],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginError = ""
+  loginForm = this.formBuilder.group({
+    email:['asd@gmail.com', [Validators.required, Validators.email]],
+    password:['123', [Validators.required]]
+  })
+
+  constructor (private formBuilder: FormBuilder, private router:Router, private authService: AuthService) {}
+
+  get email() {
+    return this.loginForm.controls.email
+  }
+
+  get password() {
+    return this.loginForm.controls.password
+  }
+
+  login() {
+    // Si no es valido salgo
+    if (!this.loginForm.valid) {
+      this.loginForm.markAllAsTouched() // Marcar todas para ver los errores
+      return;
+    }
+
+    this.authService.login(this.loginForm.value as LoginRequest).subscribe({
+      error: (errorData) => {
+        this.loginError = errorData
+        this.router.navigateByUrl("/login")
+      },
+      complete: () => {
+        this.loginForm.reset()
+        this.router.navigateByUrl("/home")
+      }
+    })
+
+  }
 }
