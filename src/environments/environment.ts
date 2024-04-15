59337641a23dd651b36541f71d7eb04671d75d74
FCT-Marcos <164381752+FCT-Marcos@users.noreply.github.com>
2024-04-15 22:39:48 +0100

    Fixed env api urls vars

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  URL_API_AUTH: 'http://localhost:8080/auth/',
-  URL_API: 'http://localhost:8080/api/v1/',
+  URL_API_AUTH: 'http://localhost:8080/api/v1/auth',
+  URL_API: 'http://localhost:8080/api/v1',
 };
