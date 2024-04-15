13f2fef36c72d9f8cd1af4a7320dcb8f4fc69b5b
FCT-Marcos <164381752+FCT-Marcos@users.noreply.github.com>
2024-04-15 22:40:16 +0100

    Improved/fixed auth logic

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, throwError, tap, map } from 'rxjs';
+import { LoginRequest } from '../../interfaces/LoginRequest';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { RegisterRequest } from '../../interfaces/RegisterRequest';
+import { Router } from '@angular/router';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -10,30 +13,52 @@ const httpOptions = {
 @Injectable({ providedIn: 'root' })
 export class AuthService {
 
-  constructor(private http: HttpClient) {}
+  isUserLogged: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  userToken: BehaviorSubject<String> = new BehaviorSubject<String>("");
 
-  login(username: string, password: string): Observable<any> {
-    return this.http.post(`${environment.URL_API_AUTH}/login`,
-      {
-        username,
-        password,
-      },
-      httpOptions
+  constructor(private http: HttpClient, private router:Router) {
+    this.isUserLogged = new BehaviorSubject<boolean>(sessionStorage.getItem("access_token") != null);
+    this.userToken = new BehaviorSubject<String>(sessionStorage.getItem("access_token") || "");
+  }
+
+  login(credentials:LoginRequest): Observable<any> {
+    return this.http.post<any>(`${environment.URL_API_AUTH}/login`, credentials, httpOptions).pipe(
+      tap((userData) => {
+        sessionStorage.setItem("access_token", userData.token);
+        this.userToken.next(userData.token);
+        this.isUserLogged.next(true);
+      }),
+      map((userData)=> userData.token),
+      catchError(this.handleError)
     );
   }
 
-  register(username: string, email: string, password: string): Observable<any> {
-    return this.http.post(`${environment.URL_API_AUTH}/register`,
-      {
-        username,
-        email,
-        password,
-      },
-      httpOptions
+  register(credentials:RegisterRequest): Observable<any> {
+    return this.http.post<any>(`${environment.URL_API_AUTH}/register`, credentials, httpOptions).pipe(
+      tap((userData) => {
+        sessionStorage.setItem("access_token", userData.token);
+        this.userToken.next(userData.token);
+        this.isUserLogged.next(true);
+      }),
+      map((userData)=> userData.token),
+      catchError(this.handleError)
     );
   }
 
-  // logout(): Observable<any> {
-  // }
+  logout(): void {
+    sessionStorage.removeItem("access_token");
+    this.isUserLogged.next(false)
+    this.router.navigateByUrl("/login")
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error("Se ha producido un error ", error.error)
+    }else{
+      console.error("Error status code ", error.status)
+    }
+
+    return throwError(() => new Error("Algo falló, Por favor intentelo de nuevo"))
+  }
 
 }
