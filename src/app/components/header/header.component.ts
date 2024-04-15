bf654b1ebd653ae22a4ec0e2bb4b9cb74f5eb2a4
FCT-Marcos <164381752+FCT-Marcos@users.noreply.github.com>
2024-04-15 22:32:37 +0100

    Improved header, added user logged check

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +9,21 @@ import { RouterLink } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   title = 'Blog Angular';
+  isUserLogged: boolean = false
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.isUserLogged.subscribe({
+      next: (logged) => {
+        this.isUserLogged = logged;
+      }
+    })
+  }
+
+  logout(): void {
+    if (!this.isUserLogged) return;
+    this.authService.logout()
+  }
 }
