4b865366f2393e4cf3cf2f36235f8fd5b6fa44e0
FCT-Marcos <164381752+FCT-Marcos@users.noreply.github.com>
2024-04-15 22:33:57 +0100

    Fixed auth guard

--- src/app/guards/authGuard/auth.guard.ts
+++ src/app/guards/authGuard/auth.guard.ts
@@ -1,9 +1,13 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
+  const router = inject(Router);
 
-  return authService.isUserLogged;
+  if (authService.isUserLogged.value) return true;
+
+  router.navigateByUrl("/login")
+  return false;
 };
